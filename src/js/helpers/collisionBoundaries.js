9d29d7f446356f08ee0622532dc70b418a49de51
Igor Klyopov <shoot8202@gmail.com>
2023-11-19 16:31:46 +0200

    update collisionBoundaries

--- src/js/helpers/collisionBoundaries.js
+++ src/js/helpers/collisionBoundaries.js
@@ -1,26 +1,28 @@
-import { Boundary } from './components/classes/Boundary';
-import { make2DArray } from '../utils/make2DArray';
-import { collisionMainData } from '../data/collisions/collisionMain';
-import { GAME_MAP_POSITION_DEFAULT } from '../data/constants';
+import { Boundary } from '../components/Boundary.js';
+import { make2DArray } from '../utils/make2DArray.js';
+import { collisionMainData } from '../data/collisions/collisionMain.js';
+import { COLLISION_OBJECTS_TYPES } from '../data/constants.js';
 
 const { map, width, tileSize } = collisionMainData;
 const collisionsMap = make2DArray(map, width);
-
 const collisionBoundaries = [];
 
-collisionsMap.forEach((row, i) => {
-  row.forEach((item, j) => {
-    const collisionBoundary = new Boundary({
-      position: {
-        x: j * tileSize + GAME_MAP_POSITION_DEFAULT.x,
-        y: i * tileSize + GAME_MAP_POSITION_DEFAULT.y,
-      },
-      width: tileSize,
-      height: tileSize,
-      color: 'rgba(244,8,222,0.5)',
-    });
+collisionsMap.forEach((row, y) => {
+  row.forEach((item, x) => {
+    if (item !== 0) {
+      const collisionBoundary = new Boundary({
+        name: COLLISION_OBJECTS_TYPES[item],
+        position: {
+          x: x * tileSize,
+          y: y * tileSize,
+        },
+        width: tileSize,
+        height: tileSize,
+        color: 'rgba(244,8,222,0.5)',
+      });
 
-    if (item === 1) collisionBoundaries.push(collisionBoundary);
+      collisionBoundaries.push(collisionBoundary);
+    }
   });
 });
 
