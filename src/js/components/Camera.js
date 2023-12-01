6cad4af3a5f65793299c585c0af54efcebdf0a35
Igor Klyopov <shoot8202@gmail.com>
2023-12-01 16:27:18 +0200

    replace hardcodet values at Camera to game constants

--- src/js/components/Camera.js
+++ src/js/components/Camera.js
@@ -1,6 +1,11 @@
 import { GameObject } from './GameObject.js';
 import { events } from './Events.js';
 import { Vector2 } from './Vector2.js';
+import { GAME_CANVAS_WIDTH, GAME_CANVAS_HEIGHT } from '../data/constants.js';
+import { assetsData } from '../data/assetsData.js';
+import { findAssetByName } from '../utils/findAssetByName.js';
+
+const heroSpriteData = findAssetByName(assetsData, 'hero');
 
 export class Camera extends GameObject {
   constructor() {
@@ -8,11 +13,11 @@ export class Camera extends GameObject {
 
     events.on('HERO_POSITION', this, (heroPosition) => {
       // Create a new position based on the hero's position
-      const personHalf = 8;
-      const canvasWidth = 320;
-      const canvasHeight = 180;
-      const halfWidth = -personHalf + canvasWidth / 2;
-      const halfHeight = -personHalf + canvasHeight / 2;
+      const heroHalf = heroSpriteData.frameSize.width / 2;
+      const canvasWidth = GAME_CANVAS_WIDTH;
+      const canvasHeight = GAME_CANVAS_HEIGHT;
+      const halfWidth = -heroHalf + canvasWidth / 2;
+      const halfHeight = -heroHalf + canvasHeight / 2;
       this.position = new Vector2(
         -heroPosition.x + halfWidth,
         -heroPosition.y + halfHeight,
